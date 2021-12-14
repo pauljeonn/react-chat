0eefa2d23da116fe2baf620c9eac4a5958b6e8f8
Paul Jeon <pauljeonn@gmail.com>
2021-12-15 06:38:15 +0900

    socket.io setup for client and server

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,39 @@
 import './App.css';
+import { useState } from 'react';
+import io from 'socket.io-client';
+
+const socket = io.connect('http://localhost:4000');
 
 function App() {
-	return <div className="App">Chat App</div>;
+	const [username, setUsername] = useState('');
+	const [room, setRoom] = useState('');
+
+	const joinRoom = () => {
+		if (username && room) {
+			socket.emit('joinRoom', room); // emit joinRoom with room
+		}
+	};
+
+	return (
+		<div className="App">
+			<h3>Join Chat</h3>
+			<input
+				type="text"
+				placeholder="유저 이름"
+				onChange={(e) => {
+					setUsername(e.target.value);
+				}}
+			/>
+			<input
+				type="text"
+				placeholder="채팅방 이름"
+				onChange={(e) => {
+					setRoom(e.target.value);
+				}}
+			/>
+			<button onClick={joinRoom}>Join Room</button>
+		</div>
+	);
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -11,17 +11,23 @@ const server = http.createServer(app); // express 서버 생성
 // pass server & object for cors (cors 이슈 해결)
 const io = new Server(server, {
 	cors: {
-		origin: 'http://localhost:4000',
+		origin: 'http://localhost:3000',
 	},
 });
 
 // socket io connection 이벤트 핸들링
 io.on('connection', (socket) => {
-	console.log(socket.id);
+	console.log(`User connected: ${socket.id}`);
+
+	// joinRoom 이벤트 - join with id
+	socket.on('joinRoom', (id) => {
+		socket.join(id);
+		console.log(`User with ID: ${socket.id} joined room: ${id}`);
+	});
 
 	// disconnect 이벤트 핸들링
 	socket.on('disconnect', () => {
-		console.log('User Disconnected', socket.id);
+		console.log('User Disconnected: ', socket.id);
 	});
 });
 
